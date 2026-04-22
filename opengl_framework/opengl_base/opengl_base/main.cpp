5fb05263261cef32a85bd112e5c591a487fd0e84
MattBergstrom7 <matt.bergstrom@student.ucll.be>
2026-04-22 14:09:15 +0200

    fixed issue movement camera

--- opengl_framework/opengl_base/opengl_base/main.cpp
+++ opengl_framework/opengl_base/opengl_base/main.cpp
@@ -264,33 +264,7 @@ int main()
     return 0;
 }
 
-void processInput(GLFWwindow* window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
 
-    if (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS && !tKeyPressed)
-    {
-        useShipCamera = !useShipCamera;
-        tKeyPressed = true;
-        firstMouse = true;   
-    }
-
-    if (glfwGetKey(window, GLFW_KEY_T) == GLFW_RELEASE)
-        tKeyPressed = false;
-
-    if (useShipCamera)
-        return;  
-
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        camera.ProcessKeyboard(FORWARD, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        camera.ProcessKeyboard(BACKWARD, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        camera.ProcessKeyboard(LEFT, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        camera.ProcessKeyboard(RIGHT, deltaTime);
-}
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -321,4 +295,32 @@ void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
     camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS && !tKeyPressed)
+    {
+        useShipCamera = !useShipCamera;
+        tKeyPressed = true;
+        firstMouse = true;  
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_T) == GLFW_RELEASE)
+        tKeyPressed = false;
+
+    if (useShipCamera)
+        return;  
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
 }
